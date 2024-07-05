a70075db8064c9771f9a87b7b8d15c46c41f547a
Chacrit <cv.time.2005@gmail.com>
2024-07-05 10:38:27 +0700

    This 3rd Status

--- Test1.java
+++ Test1.java
@@ -5,5 +5,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("A cat is on a mat.");
+        System.out.println("A cat is on a table");
     }
 }
